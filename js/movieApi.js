0eb16a46122a217c0a1100d84a99cb47d24faf3a
Conor Koritor <ckoritor@gmail.com>
2023-11-30 23:10:06 +0000

    Got bootstrap cards working with the search

--- js/movieApi.js
+++ js/movieApi.js
@@ -36,14 +36,40 @@ function populateGames(obj)
 {  
     const searchedGames = document.getElementById("gameDisplay");
 
-    let myDiv = document.createElement('div');
-    let gameTitle = document.createElement('h1');
-    let cheapestPrice = document.createElement('h3');
+    let card = document.createElement('div');
+    card.className= 'card mb-3';
+    card.style = 'max-width: 540px;';
 
+    let row = document.createElement('div');
+    row.className='row g-0';
+
+    let col1 = document.createElement('div');
+    col1.className = 'col-md-4';
+
+    let coverArt = document.createElement('img');
+    coverArt.src = obj['thumb'];
+    coverArt.className = "img-fluid rounded-start";
+
+    let col2 = document.createElement('div');
+    col2.className = 'col-md-8';
+
+    let cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+
+    let gameTitle = document.createElement('h5');
+    gameTitle.className = 'card-title';
     gameTitle.innerHTML = obj['external'];
+
+    let cheapestPrice = document.createElement('p');
+    cheapestPrice.className='card-text';
     cheapestPrice.innerHTML = obj['cheapest'];
 
-    myDiv.appendChild(gameTitle);
-    myDiv.appendChild(cheapestPrice);
-    searchedGames.appendChild(myDiv);
+    col1.appendChild(coverArt);
+    cardBody.appendChild(gameTitle);
+    cardBody.appendChild(cheapestPrice);
+    col2.appendChild(cardBody);
+    row.appendChild(col1);
+    row.appendChild(col2);
+    card.appendChild(row);
+    searchedGames.appendChild(card);
 }
