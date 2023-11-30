33e94599d1e2d98b24b2c37f39f2a5d82bbce257
Conor Koritor <ckoritor@gmail.com>
2023-11-30 10:08:11 +0000

    Got Search functionality working

--- js/movieApi.js
+++ js/movieApi.js
@@ -1,28 +1,29 @@
 (function(){
-    const searchGameText = document.getElementById("searchGame");
-    document.getElementById("submitSearchGame").addEventListener("click", searchGame())
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'c8f1aa5ccemshe1e6d2359ab689ap1a5684jsn683ee4c6ae81',
+            'X-RapidAPI-Host': 'cheapshark-game-deals.p.rapidapi.com'
+        }
+    };
+    document.getElementById("submitSearchGame").addEventListener("click", () => searchGame(options));
+    
 }());
 
-
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'c8f1aa5ccemshe1e6d2359ab689ap1a5684jsn683ee4c6ae81',
-		'X-RapidAPI-Host': 'cheapshark-game-deals.p.rapidapi.com'
-	}
-};
-
-async function searchGame(){
-    const url = `https://cheapshark-game-deals.p.rapidapi.com/games?title=${searchGameText.value}&exact=0&limit=60`;
+async function searchGame(options){
+    let searchGameText = document.getElementById("searchGame").value;
+    let seperatedSearchGameText = searchGameText.split(" ");
+    let combinedSearchGameText = seperatedSearchGameText.join("%20"); 
+    let url = `https://cheapshark-game-deals.p.rapidapi.com/games?title=${combinedSearchGameText}&exact=0&limit=60`;
     try {
-	const response = await fetch(url, options);
+	let response = await fetch(url, options);
 	
 	if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
-    const result = await response.json();
-
+    let result = await response.json();
     
+    console.log(result);
     } catch (error) {
 	console.error(error);
     }
